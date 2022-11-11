a68633aa381ae9ca1b266d46dd2287d170105092
Azaya89 <akoritaisaiah@gmail.com>
2022-11-11 14:12:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# World Earthquake Data
+
+
+This is a project that shows the magnitude of earthquakes around the world for the past month.
+
+It was plotted using Plotly and each point in the map has information about each earthquake, including the geo-location and place name. The size and colour intensity of the dots represent the magnitude of the earthquales.
+
+
+The raw data was gotten from [here](https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson).
+
+        
---
+++ e_quake.py
@@ -0,0 +1,44 @@
+import json
+from plotly.graph_objects import Layout
+from plotly import offline
+
+filename = "all_month.json"
+
+with open(filename, encoding="utf-8") as f:
+    earthquake_data = json.load(f)
+
+
+title = earthquake_data["metadata"]["title"]
+
+data_file = earthquake_data["features"]
+
+
+mags, lons, lats, infos = [], [], [], []
+
+for i in data_file:
+    mags.append(i["properties"]["mag"])
+    lons.append(i["geometry"]["coordinates"][0])
+    lats.append(i["geometry"]["coordinates"][1])
+    infos.append(i["properties"]["title"])
+
+
+data = [
+    {
+        "type": "scattergeo",
+        "lon": lons,
+        "lat": lats,
+        "text": infos,
+        "marker": {
+            "size": [3 * abs(mag) for mag in mags],
+            "color": mags,
+            "colorscale": "YlOrRd",
+            "colorbar": {"title": "Magnitude."},
+        },
+    }
+]
+
+my_layout = Layout(title=title)
+
+fig = {"data": data, "layout": my_layout}
+
+offline.plot(fig)
